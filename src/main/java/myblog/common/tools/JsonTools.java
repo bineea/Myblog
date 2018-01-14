8047f89f9d285acc8ac34664eb7bb2065015c735
bineea <981344903@qq.com>
2018-01-14 23:41:32 +0800

    jackson

--- src/main/java/myblog/common/tools/JsonTools.java
+++ src/main/java/myblog/common/tools/JsonTools.java
@@ -1,5 +1,22 @@
 package myblog.common.tools;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class JsonTools {
 
+	public static final String FILTER_NAME = "myFilter";
+	protected static Logger logger = LoggerFactory.getLogger(JsonTools.class);
+	protected static ObjectMapper mapper ;
+	
+	static{
+		mapper = new ObjectMapper();
+		
+	}
+	
+	public static void main(String[] args){
+		logger.info(" ‰≥ˆ»’÷æ£°£°£°");
+	}
 }
