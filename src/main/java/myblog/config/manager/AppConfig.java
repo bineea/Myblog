aa0cdddb19dd5969ee147aae413ff08b13b7e5d0
bineea <981344903@qq.com>
2018-01-15 19:29:44 +0800

    加载配置文件

--- src/main/java/myblog/config/manager/AppConfig.java
+++ src/main/java/myblog/config/manager/AppConfig.java
@@ -1,8 +1,12 @@
 package myblog.config.manager;
 
+import org.springframework.beans.factory.config.PropertiesFactoryBean;
 import org.springframework.context.annotation.AdviceMode;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -20,5 +24,17 @@ public class AppConfig
 {
 	public static final String APP_NAME = "myblog";
 	
+	@Bean
+	public PropertiesFactoryBean initProperties()
+	{
+		PropertiesFactoryBean propertiesFactory = new PropertiesFactoryBean();
+		Resource[] fileResource = {
+				new FileSystemResource(System.getProperty("user.dir")+"\\src\\main\\resources\\config\\hibernate-database.properties")
+//				new FileSystemResource("config/log4j.properties")
+		};
+		propertiesFactory.setLocations(fileResource);
+		propertiesFactory.setFileEncoding("utfe-8");
+		return propertiesFactory;
+	}
 	
 }
--- src/main/java/myblog/web/WelcomeController.java
+++ src/main/java/myblog/web/WelcomeController.java
@@ -2,6 +2,7 @@ package myblog.web;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +11,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class WelcomeController 
 {
+	@Value("databaseProperties.name")
+	private String name;
+	
 	@RequestMapping(value="/common/welcome", method=RequestMethod.GET)
 	public String setupForm(HttpServletRequest request, Model model)
 	{
 		System.out.println("«Î«Ûwelcome“≥√Ê");
+		System.out.println("name");
 		return "welcome";
 	}
 }
