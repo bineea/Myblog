a50f46426c36f27045e20459cb9a295668835143
bineea <981344903@qq.com>
2018-01-15 23:35:04 +0800

    配置数据库连接池

--- src/main/java/myblog/config/manager/AppConfig.java
+++ src/main/java/myblog/config/manager/AppConfig.java
@@ -1,14 +1,18 @@
 package myblog.config.manager;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.PropertiesFactoryBean;
 import org.springframework.context.annotation.AdviceMode;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.alibaba.druid.pool.DruidDataSource;
 
 @Configuration
 //注解开启对Spring Data JPA Repostory的支持
@@ -24,17 +28,64 @@ public class AppConfig
 {
 	public static final String APP_NAME = "myblog";
 	
-	@Bean
-	public PropertiesFactoryBean initProperties()
+	@Bean(name = "databaseProperties")
+	public PropertiesFactoryBean loadDatabaseProperties()
 	{
 		PropertiesFactoryBean propertiesFactory = new PropertiesFactoryBean();
-		Resource[] fileResource = {
-				new FileSystemResource(System.getProperty("user.dir")+"\\src\\main\\resources\\config\\hibernate-database.properties")
-//				new FileSystemResource("config/log4j.properties")
-		};
-		propertiesFactory.setLocations(fileResource);
-		propertiesFactory.setFileEncoding("utfe-8");
+		propertiesFactory.setLocation(new ClassPathResource("config/hibernate-database.properties"));
+		propertiesFactory.setFileEncoding("utf-8");
 		return propertiesFactory;
 	}
 	
+	@Bean(name = "log4jProperties")
+	public PropertiesFactoryBean loadLog4jProperties()
+	{
+		PropertiesFactoryBean propertiesFactory = new PropertiesFactoryBean();
+		propertiesFactory.setLocation(new ClassPathResource("config/log4j.properties"));
+		propertiesFactory.setFileEncoding("utf-8");
+		return propertiesFactory;
+	}
+	
+	@Value("#{databaseProperties.driverClassName}")
+	private String driverClassName;
+	@Value("#{databaseProperties.url}")
+	private String jdbcUrl;
+	@Value("#{databaseProperties.username}")
+	private String username;
+	@Value("#{databaseProperties.password}")
+	private String password;
+	@Value("#{databaseProperties.dialect}")
+	private String dialect;
+	@Value("#{databaseProperties.validationQuery}")
+	private String validationQuery;
+	
+	@Bean(name = "dataSource", destroyMethod = "close")
+	public DataSource initDataSource()
+	{
+		DruidDataSource dataSource = new DruidDataSource();
+		dataSource.setDriverClassName(driverClassName);
+		dataSource.setUrl(jdbcUrl);
+		dataSource.setUsername(username);
+		dataSource.setPassword(password);
+		//初始化连接数
+		dataSource.setInitialSize(5);
+		//最小空闲连接数
+		dataSource.setMinIdle(1);
+		//最大活动连接数
+		dataSource.setMaxActive(100);
+		//最大连接等待时间
+		dataSource.setMaxWait(60000);
+		//SQL查询,用来验证从连接池取出的连接,在将连接返回给调用者之前.如果指定,则查询必须是一个SQL SELECT并且必须返回至少一行记录 
+		dataSource.setValidationQuery(validationQuery);
+		//是否在从连接池中取出连接前进行检验,如果检验失败,则从连接池中去除连接并尝试取出另一个；若设置为true，validationQuery参数必须设置为非空字符串
+		dataSource.setTestOnBorrow(false);
+		//是否在归还到池中前进行检验；若设置为true，validationQuery参数必须设置为非空字符串
+		dataSource.setTestOnReturn(false);
+		//连接是否被空闲连接回收器(如果有)进行检验.如果检测失败,则连接将被从池中去除；若设置为true，validationQuery参数必须设置为非空字符串
+		dataSource.setTestWhileIdle(true);
+		// 打开PSCache，并且指定每个连接上PSCache的大小
+		dataSource.setPoolPreparedStatements(true);
+		dataSource.setMaxPoolPreparedStatementPerConnectionSize(20);
+		return dataSource;
+	}
 }
