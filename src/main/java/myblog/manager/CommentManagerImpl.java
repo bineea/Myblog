6318b382c6a426d37baa06bfcf320daeba4941f4
bineea <981344903@qq.com>
2018-01-20 23:46:04 +0800

    jsonTools

--- src/main/java/myblog/manager/CommentManagerImpl.java
+++ src/main/java/myblog/manager/CommentManagerImpl.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import myblog.common.tools.JsonTools;
 import myblog.dao.entity.Comment;
 import myblog.dao.repo.jpa.CommentRepo;
 
@@ -17,15 +18,21 @@ public class CommentManagerImpl extends AbstractManager implements CommentManage
 	@Override
 	public void test()
 	{
-		Optional<Comment> commentOptional = commentRepo.findById("123123");
-		commentOptional.ifPresent(comment -> {
-			System.out.println(comment.toString());
-		});
-		if(!commentOptional.isPresent())
-			System.out.println("未查询到数据");
-			logger.info("info---{} 未查询到数据", "commentOptional");
-			logger.debug("debug---{} 未查询到数据", "commentOptional");
-			logger.warn("warn---{} 未查询到数据", "commentOptional");
-			logger.error("error---{} 未查询到数据", "commentOptional");
+		try {
+			Optional<Comment> commentOptional = commentRepo.findById("123123");
+			commentOptional.ifPresent(comment -> {
+				System.out.println(comment.toString());
+			});
+			if(!commentOptional.isPresent())
+				System.out.println("未查询到数据");
+				logger.info("info---{} 未查询到数据", "commentOptional");
+				logger.debug("debug---{} 未查询到数据", "commentOptional");
+				logger.warn("warn---{} 未查询到数据", "commentOptional");
+				logger.error("error---{} 未查询到数据", "commentOptional");
+				logger.info(JsonTools.entityToJson(new Comment()));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 }
