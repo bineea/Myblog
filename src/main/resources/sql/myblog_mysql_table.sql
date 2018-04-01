de61e421ad40fd1f6baf5805e214db9bffea76bd
bineea <981344903@qq.com>
2018-04-01 23:05:38 +0800

    文章/类别/文章类别关联表

--- src/main/resources/sql/myblog_mysql_table.sql
+++ src/main/resources/sql/myblog_mysql_table.sql
@@ -93,3 +93,54 @@ UNIQUE INDEX `loginName` (`login_name`) USING BTREE,
 CONSTRAINT `roleId` FOREIGN KEY (`role_id`) REFERENCES `blog_role` (`id`)
 )
 ENGINE=InnoDB DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci COMMENT='用户表，用于保存用户信息。';
+
+DROP TABLE IF EXISTS `blog_category`;
+
+CREATE TABLE `blog_category` (
+`id`  varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
+`name`  varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '类别名称' ,
+`icon`  blob NULL COMMENT '图标' ,
+`content_count`  int(11) NOT NULL COMMENT '文章数量' ,
+`flag`  varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '标识' ,
+`lat`  double NULL DEFAULT NULL COMMENT '纬度' ,
+`lng`  double NULL DEFAULT NULL COMMENT '经度' ,
+PRIMARY KEY (`id`)
+)
+ENGINE=InnoDB DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci COMMENT='类别表';
+
+DROP TABLE IF EXISTS `blog_content_category_mapping`;
+
+CREATE TABLE `blog_content_category_mapping` (
+`id`  varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
+`content_id`  varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '文章id' ,
+`category_id`  varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '类别id' ,
+PRIMARY KEY (`id`),
+FOREIGN KEY (`category_id`) REFERENCES `blog_category` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+FOREIGN KEY (`content_id`) REFERENCES `blog_content` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+INDEX `content_id` (`content_id`) USING BTREE ,
+INDEX `category_id` (`category_id`) USING BTREE ,
+UNIQUE INDEX `content_category` (`content_id`,`category_id`) USING BTREE,
+)
+ENGINE=InnoDB DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci COMMENT='文章与类别对应关联表';
+
+DROP TABLE IF EXISTS `blog_content`;
+
+CREATE TABLE `blog_content` (
+`id`  varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
+`title`  varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '标题' ,
+`text`  longtext CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '内容' ,
+`summany`  varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '摘要' ,
+`markdown_enable`  tinyint(4) NOT NULL COMMENT '是否启用markdown' ,
+`thumbnail`  blob NULL COMMENT '缩略图' ,
+`content_status`  varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '文章状态' ,
+`vote_up`  int(11) NOT NULL COMMENT '“顶”的数量' ,
+`comment_count`  int(11) NOT NULL COMMENT '评论数量' ,
+`view_count`  int(11) NOT NULL COMMENT '访问量' ,
+`create_time`  datetime NOT NULL COMMENT '创建时间' ,
+`update_time`  datetime NOT NULL COMMENT '更新时间' ,
+`flag`  varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '标识' ,
+`lat`  double NULL DEFAULT NULL COMMENT '纬度' ,
+`lng`  double NULL DEFAULT NULL COMMENT '经度' ,
+PRIMARY KEY (`id`)
+)
+ENGINE=InnoDB DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci COMMENT='文章表';
