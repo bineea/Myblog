85ac915822f9a37e6bae2a08ccb70b90ad311ac8
bineea <981344903@qq.com>
2018-04-11 22:01:31 +0800

    通过loginName查询用户

--- src/main/java/myblog/dao/repo/jpa/UserRepo.java
+++ src/main/java/myblog/dao/repo/jpa/UserRepo.java
@@ -1,5 +1,8 @@
 package myblog.dao.repo.jpa;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
@@ -10,4 +13,9 @@ public interface UserRepo extends JpaRepository<User, String>, JpaSpecificationE
 
 	@Query(value = "select u from User u where u.loginName = ?1 and u.passwd = ?2")
 	User findByLoginNamePasswd(String loginName, String passwd);
+	
+	Page<User> findAll(Specification<User> spec, Pageable pageable);
+	
+	@Query(value = "select u from User u where u.loginName = ?1")
+	User findByLoginName(String loginName);
 }
