38ab957f995f37363b02eb163773bfc88ed1de2c
bineea <981344903@qq.com>
2018-04-23 15:55:22 +0800

    404和无权限跳转

---
+++ src/main/java/myblog/web/AclController.java
@@ -0,0 +1,12 @@
+package myblog.web;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author Administrator
+ * 无权限跳转、退出
+ */
+@Controller
+public class AclController extends AbstractController {
+
+}
