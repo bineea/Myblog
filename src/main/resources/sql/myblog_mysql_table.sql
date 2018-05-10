b11777bc870dfccc53b0837527d9a3c08ff89be5
bineea <981344903@qq.com>
2018-05-10 20:02:36 +0800

    content表修改字段“thumbnail”为“cover”

--- src/main/resources/sql/myblog_mysql_table.sql
+++ src/main/resources/sql/myblog_mysql_table.sql
@@ -119,7 +119,7 @@ FOREIGN KEY (`category_id`) REFERENCES `blog_category` (`id`) ON DELETE RESTRICT
 FOREIGN KEY (`content_id`) REFERENCES `blog_content` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
 INDEX `content_id` (`content_id`) USING BTREE ,
 INDEX `category_id` (`category_id`) USING BTREE ,
-UNIQUE INDEX `content_category` (`content_id`,`category_id`) USING BTREE,
+UNIQUE INDEX `content_category` (`content_id`,`category_id`) USING BTREE
 )
 ENGINE=InnoDB DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci COMMENT='文章与类别对应关联表';
 
@@ -131,7 +131,7 @@ CREATE TABLE `blog_content` (
 `text`  longtext CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '内容' ,
 `summany`  varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '摘要' ,
 `markdown_enable`  tinyint(4) NOT NULL COMMENT '是否启用markdown' ,
-`thumbnail`  blob NULL COMMENT '缩略图' ,
+`cover`  blob NULL COMMENT '封面' ,
 `content_status`  varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '文章状态' ,
 `vote_up`  int(11) NOT NULL COMMENT '“顶”的数量' ,
 `comment_count`  int(11) NOT NULL COMMENT '评论数量' ,
