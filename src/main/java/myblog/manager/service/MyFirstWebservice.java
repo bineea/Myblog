60339487ec47c26760796259ab74f2488be213f3
bineea <981344903@qq.com>
2018-11-20 00:13:55 +0800

    cxf

--- src/main/java/myblog/manager/service/MyFirstWebservice.java
+++ src/main/java/myblog/manager/service/MyFirstWebservice.java
@@ -1,6 +1,11 @@
 package myblog.manager.service;
 
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService
 public interface MyFirstWebservice {
 
+	@WebMethod
 	public String sayHello();
 }
--- src/main/java/myblog/manager/service/MyFirstWebserviceImpl.java
+++ src/main/java/myblog/manager/service/MyFirstWebserviceImpl.java
@@ -1,10 +1,15 @@
 package myblog.manager.service;
 
+import javax.jws.WebMethod;
 import javax.jws.WebService;
 
+import org.springframework.stereotype.Service;
+
 @WebService
+@Service
 public class MyFirstWebserviceImpl implements MyFirstWebservice {
 
+	@WebMethod
 	@Override
 	public String sayHello(){
 		
