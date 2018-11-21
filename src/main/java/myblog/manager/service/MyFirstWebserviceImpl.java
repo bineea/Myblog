9c1ca5fa82733b59d64bb2cb78deead46b720dab
bineea <981344903@qq.com>
2018-11-21 22:19:32 +0800

    webservice方法

--- src/main/java/myblog/manager/service/MyFirstWebserviceImpl.java
+++ src/main/java/myblog/manager/service/MyFirstWebserviceImpl.java
@@ -5,14 +5,16 @@ import javax.jws.WebService;
 
 import org.springframework.stereotype.Service;
 
+import myblog.manager.AbstractManager;
+
 @WebService
 @Service
-public class MyFirstWebserviceImpl implements MyFirstWebservice {
+public class MyFirstWebserviceImpl extends AbstractManager implements MyFirstWebservice {
 
 	@WebMethod
 	@Override
 	public String sayHello(){
-		
+		logger.info("----->>>>进入sayHello方法------");
 		return "Hello World!!!";
 	}
 }
