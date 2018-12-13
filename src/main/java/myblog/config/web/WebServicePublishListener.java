08bc8b7c1e56f013b65e513f94adb1ff1958bc97
bineea <981344903@qq.com>
2018-12-13 21:17:10 +0800

    webService服务与web服务的端口确保不一致

--- src/main/java/myblog/config/web/WebServicePublishListener.java
+++ src/main/java/myblog/config/web/WebServicePublishListener.java
@@ -12,9 +12,11 @@ import myblog.manager.service.MyFirstWebserviceImpl;
 public class WebServicePublishListener extends AbstractManager implements ServletContextListener {
 
 	//启动web应用时，系统调用Listener的该方法
+	//webService服务与web服务的端口确保不一致
+	@Override
 	public void contextInitialized(ServletContextEvent sce) {
 		logger.info("-------------Start publish webService-------------");
-		Endpoint.publish("http://127.0.0.1:8080/myblog/webservice/myFirst", new MyFirstWebserviceImpl());
+		Endpoint.publish("http://127.0.0.1:8180/myblog/webservice/myFirst", new MyFirstWebserviceImpl());
 		logger.info("-------------End publish webService-------------");
 	}
 }
