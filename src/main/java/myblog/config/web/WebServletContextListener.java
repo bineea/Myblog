44d8e854722b0fdd0839b871d3464b70f7a1dd2f
bineea <981344903@qq.com>
2019-01-08 13:43:30 +0800

    webservice服务无法通过listener发布

--- src/main/java/myblog/config/web/WebServletContextListener.java
+++ src/main/java/myblog/config/web/WebServletContextListener.java
@@ -7,15 +7,13 @@ import javax.servlet.annotation.WebListener;
 import myblog.manager.AbstractManager;
 
 @WebListener
-public class WebServicePublishListener extends AbstractManager implements ServletContextListener {
+public class WebServletContextListener extends AbstractManager implements ServletContextListener {
 
 	//启动listener的时候还没有初始化bean工厂，不可能注入什么东西
 	//启动web应用时，系统调用Listener的该方法
-	//webService服务与web服务的端口确保不一致
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
-		logger.info("-------------Start publish webService-------------");
-		
-		logger.info("-------------End publish webService-------------");
+		logger.info("-------------Start context init-------------");
+		logger.info("-------------End context init-------------");
 	}
 }
