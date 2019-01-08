1cbd7424909ddcd620768259c1afe84e6e515b10
bineea <981344903@qq.com>
2019-01-08 13:40:39 +0800

    启动listener的时候还没有初始化bean工厂，不可能注入什么东西，所以无法发布webservice

--- src/main/java/myblog/config/web/WebServicePublishListener.java
+++ src/main/java/myblog/config/web/WebServicePublishListener.java
@@ -3,20 +3,19 @@ package myblog.config.web;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
-import javax.xml.ws.Endpoint;
 
 import myblog.manager.AbstractManager;
-import myblog.manager.service.MyFirstWebserviceImpl;
 
 @WebListener
 public class WebServicePublishListener extends AbstractManager implements ServletContextListener {
 
+	//启动listener的时候还没有初始化bean工厂，不可能注入什么东西
 	//启动web应用时，系统调用Listener的该方法
 	//webService服务与web服务的端口确保不一致
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
 		logger.info("-------------Start publish webService-------------");
-		Endpoint.publish("http://127.0.0.1:8180/myblog/webservice/myFirst", new MyFirstWebserviceImpl());
+		
 		logger.info("-------------End publish webService-------------");
 	}
 }
