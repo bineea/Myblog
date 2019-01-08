aedbd3cf39212dcbcdb1311a70bc4a411907ccef
bineea <981344903@qq.com>
2019-01-08 13:41:01 +0800

    webservice服务

--- src/main/java/myblog/manager/service/MyFirstWebservice.java
+++ src/main/java/myblog/manager/service/MyFirstWebservice.java
@@ -1,11 +1,18 @@
 package myblog.manager.service;
 
+import java.util.List;
+
 import javax.jws.WebMethod;
 import javax.jws.WebService;
+
+import myblog.dao.entity.Role;
 
 @WebService
 public interface MyFirstWebservice {
 
 	@WebMethod
 	public String sayHello();
+	
+	@WebMethod
+	public List<Role> findRoleByName(String name);
 }
--- src/main/java/myblog/manager/service/MyFirstWebserviceImpl.java
+++ src/main/java/myblog/manager/service/MyFirstWebserviceImpl.java
@@ -1,20 +1,41 @@
 package myblog.manager.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.jws.WebMethod;
-import javax.jws.WebService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
-import myblog.manager.AbstractManager;
+import myblog.dao.entity.Role;
+import myblog.dao.repo.jpa.RoleRepo;
 
-@WebService
 @Service
-public class MyFirstWebserviceImpl extends AbstractManager implements MyFirstWebservice {
+public class MyFirstWebserviceImpl extends SpringBeanAutowiringSupport implements MyFirstWebservice {
 
+	@Autowired
+	private RoleRepo roleRepo;
+	
 	@WebMethod
 	@Override
 	public String sayHello(){
-		logger.info("----->>>>进入sayHello方法------");
+		System.out.println("----->>>>进入sayHello方法------");
 		return "Hello World!!!";
 	}
+
+	@WebMethod
+	@Override
+	public List<Role> findRoleByName(String name) {
+		Assert.hasText(name, "name不能为空");
+		System.out.println("---------------->>>name:"+name);
+		List<Role> list = roleRepo.findAll();
+		System.out.println("---------------->>>list:"+list.toString());
+		if(list == null || list.isEmpty())
+			return new ArrayList<Role>();
+		return list;
+	}
+
 }
