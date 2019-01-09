7a5a30c22160a16371505ce85ea6cf28dcb5804e
bineea <981344903@qq.com>
2019-01-09 23:15:12 +0800

    Update test.java

--- src/main/java/myblog/web/blog/test.java
+++ src/main/java/myblog/web/blog/test.java
@@ -8,7 +8,9 @@ public class test {
 
 	public List<CommentModel> commentList; 
 }
-
+//初步设想:
+//后台递归封装数据；前台递归展示数据
+//前台ajax请求，后台将数据赋值到response中
 class CommentModel {
 	private List<CommentModel> commentList;
 	private Comment comment;
